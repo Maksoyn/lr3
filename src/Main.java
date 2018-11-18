50662e309dff071a86314c8618ad599dd11680ef
Maksoyn <43723380+Maksoyn@users.noreply.github.com>
2018-11-18 23:13:56 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("commit");
-        System.out.println("Changes on GitHub1");
+        System.out.println("Changes on GitHub");
     }
 }
